816fa631bbb978e7adb50248eb0d9e8d7a9b05ea
Andrea Restello <andrea.restello@gmail.com>
2017-09-18 15:06:37 +0200

    Phpunit install

--- tests/AppBundle/Controller/ApiControllerTest.php
+++ tests/AppBundle/Controller/ApiControllerTest.php
@@ -6,13 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ApiControllerTest extends WebTestCase
 {
-    public function testIndex()
+
+	protected function setUp()
+    {   
+        require_once __DIR__.'/../../../app/AppKernel.php';
+
+        $kernel = new \AppKernel('test', true);
+        $kernel->boot();
+        $container = $kernel->getContainer();
+        $this->faker = $container->get('faker.generator');
+
+        $this->loadFixtures(array(
+            'AppBundle\DataFixtures\ORM\AuthorFixture',
+            'AppBundle\DataFixtures\ORM\QuoteFixture',
+        ));
+    }
+
+    public function testRandomSuccess()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/api/');
+        $crawler = $client->request('GET', '/api/quote/random');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+
+        $data = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('author', $data );
+        $this->assertArrayHasKey('content', $data );
+
     }
 }
